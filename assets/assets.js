09e71059a115b0fb38c603e8957523e93239c321
mosfiend <abdellaouimoss@gmail.com>
2023-09-26 00:34:27 +0100

    soyez vite

---
+++ assets/assets.js
@@ -0,0 +1,18 @@
+export const manifest = {
+    bundles: [
+        {
+            name: "background",
+            assets:
+            {
+                "road": "/assets/images/road.png",
+                "rope": "assets/pictors/road.png",
+                "building": "/assets/images/building.png",
+                "rooftile": "/assets/images/rooftile.png",
+                "pointer": "/assets/images/pointer.png",
+                "bgBuildings": "/assets/images/bg-buildings.png",
+                "bgWeather": "/assets/images/background-weather.png",
+                "around": "/assets/pictors/around.mp3",
+            }
+        }
+    ]
+}
