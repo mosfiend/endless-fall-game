f6c956c5af667a878b15a66e130d274aab4a782e
mosfiend <abdellaouimoss@gmail.com>
2024-01-12 17:36:44 +0100

    edit

--- assets/assets.js
+++ assets/assets.js
@@ -4,14 +4,14 @@ export const manifest = {
             name: "background",
             assets:
             {
-                "road": "/assets/images/road.png",
-                "rope": "assets/pictors/road.png",
-                "building": "/assets/images/building.png",
-                "rooftile": "/assets/images/rooftile.png",
-                "pointer": "/assets/images/pointer.png",
-                "bgBuildings": "/assets/images/bg-buildings.png",
-                "bgWeather": "/assets/images/background-weather.png",
-                "around": "/assets/pictors/around.mp3",
+                "road": "/images/road.png",
+                "rope": "/pictors/road.png",
+                "building": "/images/building.png",
+                "rooftile": "/images/rooftile.png",
+                "pointer": "/images/pointer.png",
+                "bgBuildings": "/images/bg-buildings.png",
+                "bgWeather": "/images/background-weather.png",
+                "around": "/pictors/around.mp3",
             }
         }
     ]
